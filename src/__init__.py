0e7177494d26458e85c310c66ed75314d9d5a898
Joern Dietze <jdi010@uit.no>
2026-05-28 13:32:03 +0200

    Remove src strucutre

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Generates mock test and training data sets.
-
-Example usage:
-  python generate_data.py --num-samples 50 --training-data data/train.csv --test-data data/test.csv
-"""
-
-__version__ = "0.1.0"
-
