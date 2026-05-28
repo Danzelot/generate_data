6d82f12ed7a37cbf79f3667b02316c6af23daa2a
Joern Dietze <jdi010@uit.no>
2026-05-28 13:24:13 +0200

    Switch to src structure

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""Generates mock test and training data sets.
+
+Example usage:
+  python generate_data.py --num-samples 50 --training-data data/train.csv --test-data data/test.csv
+"""
+
+__version__ = "0.1.0"
+
