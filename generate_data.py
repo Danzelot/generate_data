0b6d27db25e9d7d6808a1413eb2c577197f477f0
Joern Dietze <jdi010@uit.no>
2026-05-28 13:56:38 +0200

    Update version

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,7 @@ Example usage:
   python generate_data.py --num-samples 50 --training-data data/train.csv --test-data data/test.csv
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 import click
 import numpy as np
